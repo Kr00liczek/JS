8139e993f02dd7e3259b48fdc59d4ba4e031cfcc
Kr00liczek <113247851+Kr00liczek@users.noreply.github.com>
2023-09-09 19:11:45 +0200

    Add files via upload

---
+++ kozikowski_zewnetrzny.js
@@ -0,0 +1,4 @@
+//Nr w dzienniku - 8 ; Kolor tekstu - 9 ; Tekst NIE pogrubiony ; Tekst przekreślony ; Wielkość liter - 6
+
+document.write("ostatnia modyfikacja strony".fontcolor("green").strike().fontsize(6)+"<br>");
+document.write(document.lastModified);
